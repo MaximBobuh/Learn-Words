a2336690970f5be3d7ebbe7d4c1c45d223eaebd4
Maxim <progon3@mail.ru>
2015-12-19 23:41:26 +0200

    Update ProfileWidget.cpp

--- Widgets/ProfileWidget.cpp
+++ Widgets/ProfileWidget.cpp
@@ -1,4 +1,5 @@
 #include "ProfileWidget.h"
+#include "AdditionalFunctions.h"
 
 #include <QPushButton>
 #include <QBoxLayout>
@@ -13,14 +14,23 @@ ProfileWidget::ProfileWidget()
 
 void ProfileWidget::createWidget()
 {
-    QPushButton* myDict = new QPushButton("Мой словарь", this);
-    QPushButton* commDict = new QPushButton("Общий словарь", this);
-    QPushButton* training = new QPushButton("Тренировки", this);
+    QPalette pal;
+    pal.setColor(QPalette::Background, Qt::lightGray);
+    setAutoFillBackground(true);
+    setPalette(pal);
+
+    //-----------------------
+
+    QPushButton* myDict   = AdditionalFunctions::createPWButton("Мой словарь", this);
+    QPushButton* commDict = AdditionalFunctions::createPWButton("Общий словарь", this);
+    QPushButton* training = AdditionalFunctions::createPWButton("Тренировки", this);
+    QPushButton* selectProf = AdditionalFunctions::createPWButton("Выбор профиля", this);
 
     QVBoxLayout* vbox = new QVBoxLayout(this);
     vbox->addWidget(myDict);
     vbox->addWidget(commDict);
     vbox->addWidget(training);
+    vbox->addWidget(selectProf);
 
     setLayout(vbox);
     setFixedSize(350, 350);
