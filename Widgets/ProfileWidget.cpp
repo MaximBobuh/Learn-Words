26d1811c827e52e0c1eba2ab10be94255ff06880
Maxim <progon3@mail.ru>
2015-12-20 00:29:14 +0200

    Update ProfileWidget.cpp

--- Widgets/ProfileWidget.cpp
+++ Widgets/ProfileWidget.cpp
@@ -24,7 +24,12 @@ void ProfileWidget::createWidget()
     QPushButton* myDict   = AdditionalFunctions::createPWButton("Мой словарь", this);
     QPushButton* commDict = AdditionalFunctions::createPWButton("Общий словарь", this);
     QPushButton* training = AdditionalFunctions::createPWButton("Тренировки", this);
-    QPushButton* selectProf = AdditionalFunctions::createPWButton("Выбор профиля", this);
+    QPushButton* selectProf = AdditionalFunctions::createPWButton("Смена профиля", this);
+
+    connect(myDict,     SIGNAL(clicked(bool)), SLOT(slotMyDictionary()));
+    connect(commDict,   SIGNAL(clicked(bool)), SLOT(slotWholeDictionary()));
+    connect(training,   SIGNAL(clicked(bool)), SLOT(slotTraining()));
+    connect(selectProf, SIGNAL(clicked(bool)), SIGNAL(signalBack()));
 
     QVBoxLayout* vbox = new QVBoxLayout(this);
     vbox->addWidget(myDict);
@@ -34,5 +39,22 @@ void ProfileWidget::createWidget()
 
     setLayout(vbox);
     setFixedSize(350, 350);
+}
+
+//-----------------------------
+
+void ProfileWidget::slotMyDictionary()
+{
+
+}
+
+void ProfileWidget::slotWholeDictionary()
+{
+
+}
+
+void ProfileWidget::slotTraining()
+{
+
 }
 
