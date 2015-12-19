a497cac3a438405c8b71341e36eece917273b557
Maxim <progon3@mail.ru>
2015-12-20 00:30:16 +0200

    Update AdditionalFunctions.h

--- Widgets/AdditionalFunctions.h
+++ Widgets/AdditionalFunctions.h
@@ -11,8 +11,19 @@ QPushButton* createPWButton(const QString& text,QWidget* par = 0)
 {
     QPushButton* but = new QPushButton(text, par);
     but->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    but->setStyleSheet("font-size: 16pt; font-weight: bold; background-color: #E6E6FA;");
-
+    but->setStyleSheet("QPushButton {"
+                           "font-size: 16pt;"
+                           "font-weight: bold;"
+                           "background-color: #E6E6FA;"
+                           "border-radius: 7; "
+                           "color: darkslategray;"
+                       "}"
+                       "QPushButton::hover { "
+                           "background-color: darkgray;"
+                       "}"
+                       "QPushButton::pressed { "
+                           "background-color: dimgray;"
+                       "}");
     return but;
 }
 
