e1286923ac5f36edab058b9cfeb465ebc711d8c3
Maxim <progon3@mail.ru>
2015-12-23 19:18:06 +0200

    Update WordData.h

--- Data/WordData.h
+++ Data/WordData.h
@@ -5,16 +5,19 @@
 
 struct WordData
 {
-    WordData(const QString& orig = "", const QString& trsl = "", const QString& trsc = "", int d = 0, int l = 0
-             : original(orig), translation(trsl), transcription(trsc), date(d), level(l) {}
+    WordData(const QString& orig = "", const QString& trsl = "", const QString& trsc = "",
+             int d = 0, int l = 0) : original(orig), translation(trsl), transcription(trsc),
+                                     date(d), level(l) {}
 
-    bool operator <(const WordData& other) const { return original < other.original; }
+    bool operator <(const WordData& other)  const { return original < other.original; }
+    bool operator ==(const WordData& other) const { return original == other.original; }
 
     //-------------------
 
     QString original;
     QString translation;
     QString transcription;
+
     int date;
     int level;
 };
