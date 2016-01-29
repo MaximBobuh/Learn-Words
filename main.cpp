5973073146f3c43d9cf04f6eef189bd4b00bfe7f
Maxim <progon3@mail.ru>
2016-01-29 16:27:15 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "ProfileSelectWidget.h"
+#include "DictionaryView.h"
+#include "DictionaryModel.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    //ProfileSelectWidget wdg;
+
+    BaseModel* model = createModel(1);
+    model->loadFromFile("C:/Users/MBobukh/Desktop/file.txt", false);
+
+    DictionaryView wdg;
+    wdg.setModel(model);
+    wdg.show();
+
+    return a.exec();
+}
